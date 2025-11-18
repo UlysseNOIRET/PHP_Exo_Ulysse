1eb111086d32ec3c3b782c4e8befe6942107ef73
UlysseNOIRET <ulyssenoiret38@gmail.com>
2025-11-18 14:43:42 +0100

    Update Exo2.2.php

--- Exos/Exo2.2.php
+++ Exos/Exo2.2.php
@@ -1,8 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+ 
+        <form method="POST" action="index.php">
+            <label for="str">Votre phrase</label>
+            <input type="text" id="str" name="str" placeholder="Chiffre.">
+            <button type="submit">Envoyer</button>
+        </form>
+    </body>
+</html>
+ 
 <?php
-echo strrev("test test test")
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $str = $_POST['str'];
+}
 
+$length = strlen($str);
+$reversed_str = '';    
+  
+for ($i = $length - 1; $i >= 0; $i--) {
+    $reversed_str .= $str[$i];
+}
 
-
-
-
+echo $reversed_str;
 ?>
