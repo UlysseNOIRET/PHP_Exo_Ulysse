fa0158dcf1081195401ebca60ff99737d670192d
UlysseNOIRET <ulyssenoiret38@gmail.com>
2025-11-19 16:18:40 +0100

    19/11 16:18

--- Exo 5/request.php
+++ Exo 5/request.php
@@ -1,17 +1,27 @@
 <?php
 require_once("config.php");
 
-$sth = $dbh->prepare("select * from jo.100;");
+$sort = "nom";
+if (isset($_GET["sort"])){
+    $sort = $_GET["sort"];
+}
+
+$order = "asc";
+if (isset($_GET["order"])){
+    $order = $_GET["order"];
+}
+
+$sth = $dbh->prepare("SELECT * FROM jo.100 order by " .$sort. " " .$order);
 $sth->execute();
 
 $data = $sth->fetchAll(PDO::FETCH_ASSOC);
 echo"<table>
     <thead>
         <tr>
-            <th>Nom</th>
-            <th>Pays</th>
-            <th>Course</th>
-            <th>Temps</th>
+            <th>Nom<a href='http://localhost/IPSSI_PHP_Ulysse/Exo%206/request.php?sort=nom&order=asc'>⬆️</a><a href='http://localhost/IPSSI_PHP_Ulysse/Exo%206/request.php?sort=nom&order=desc'>⬇️</a></th>
+            <th>Pays<a href='http://localhost/IPSSI_PHP_Ulysse/Exo%206/request.php?sort=pays&order=asc'>⬆️</a><a href='http://localhost/IPSSI_PHP_Ulysse/Exo%206/request.php?sort=pays&order=desc'>⬇️</a></th>
+            <th>Course<a href='http://localhost/IPSSI_PHP_Ulysse/Exo%206/request.php?sort=course&order=asc'>⬆️</a><a href='http://localhost/IPSSI_PHP_Ulysse/Exo%206/request.php?sort=course&order=desc'>⬇️</a></th>
+            <th>Temps<a href='http://localhost/IPSSI_PHP_Ulysse/Exo%206/request.php?sort=temps&order=asc'>⬆️</a><a href='http://localhost/IPSSI_PHP_Ulysse/Exo%206/request.php?sort=temps&order=desc'>⬇️</a></th>
         </tr>
     </thead>";
     foreach ($data as $value) {
